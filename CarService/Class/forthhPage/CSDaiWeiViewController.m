2d60373df8be9934a737dba51b162a5d2bd04606
zzufeiying <baidu@baidutekiMacBook-Pro.local>
2014-01-10 15:39:40 +0800

    增加我的代维页面

---
+++ CarService/Class/forthhPage/CSDaiWeiViewController.m
@@ -0,0 +1,164 @@
+//
+//  CSDaiWeiViewController.m
+//  CarService
+//
+//  Created by baidu on 14-1-10.
+//  Copyright (c) 2014年 Chao. All rights reserved.
+//
+
+#import "CSDaiWeiViewController.h"
+
+@interface CSDaiWeiViewController ()
+
+@end
+
+@implementation CSDaiWeiViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView DataSource && Delegate Methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *ImageTypeCell = @"DaiWeiCell";
+    
+    UITableViewCell *cell;
+    UIImageView *iconView;
+    UILabel *title;
+    
+    cell = [tableView dequeueReusableCellWithIdentifier:ImageTypeCell];
+    if(cell == nil)
+    {
+        cell = [[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ImageTypeCell]autorelease];
+        cell.backgroundColor = [UIColor clearColor];
+        
+        iconView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 13, 18, 17)];
+        iconView.tag = 1000;
+        [cell.contentView addSubview:iconView];
+        
+        title = [[UILabel alloc] initWithFrame:CGRectMake(40, 0, 150, 44)];
+        title.backgroundColor = [UIColor clearColor];
+        title.font = [UIFont systemFontOfSize:14];
+        title.minimumFontSize = 10;
+        title.adjustsFontSizeToFitWidth = YES;
+        title.textAlignment = UITextAlignmentLeft;
+        title.textColor = [UIColor colorWithRed:0x81/255.0f green:0x81/255.0f blue:0x81/255.0f alpha:1];
+        title.tag = 100;
+        [cell.contentView addSubview:title];
+        [title release];
+        
+        UIImageView *arrowView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"membercenter_arrow.png"]];
+        arrowView.frame = CGRectMake(258, 16, 8, 11);
+        
+        [cell.contentView addSubview:arrowView];
+        [arrowView release];
+    }
+    
+    title = (UILabel *)[cell.contentView viewWithTag:100];
+    
+    UIImage *normalImage = nil;
+    UIImage *selectImage = nil;
+    
+    
+    
+    if (indexPath.row == 0)
+    {
+        normalImage = [[UIImage imageWithCGImage:[UIImage imageNamed:@"new_baoanzixun_biaogetoubu.png"].CGImage scale:2.0 orientation:UIImageOrientationUp] stretchableImageWithLeftCapWidth:25 topCapHeight:25];
+        selectImage = [[UIImage imageWithCGImage:[UIImage imageNamed:@"new_baoanzixun_biaogetoubu.png"].CGImage scale:2.0 orientation:UIImageOrientationUp] stretchableImageWithLeftCapWidth:25 topCapHeight:25];
+        title.text = @"代维状态";
+        UIImageView *icon = (UIImageView *)[cell viewWithTag:1000];
+        if (nil != icon)
+        {
+            icon.image = [UIImage imageNamed:@"new_wodedaiwei_daiweizhuangtai_tubiao.png"];
+            icon.frame = CGRectMake(10, 13, 18, 18);
+        }
+        
+    }
+    else if (indexPath.row == 1)
+    {
+        normalImage = [[UIImage imageWithCGImage:[UIImage imageNamed:@"new_baoanzixun_biaoge_zhongbu.png"].CGImage scale:2.0 orientation:UIImageOrientationUp] stretchableImageWithLeftCapWidth:25 topCapHeight:25];
+        selectImage = [[UIImage imageWithCGImage:[UIImage imageNamed:@"new_baoanzixun_biaoge_zhongbu.png"].CGImage scale:2.0 orientation:UIImageOrientationUp] stretchableImageWithLeftCapWidth:25 topCapHeight:25];
+        title.text = @"代维列表";
+        UIImageView *icon = (UIImageView *)[cell viewWithTag:1000];
+        if (nil != icon)
+        {
+            icon.image = [UIImage imageNamed:@"new_wodedaiwei_liebiao_tubiao.png"];
+            icon.frame = CGRectMake(10, 13, 18, 18);
+        }
+        
+    }
+    
+    
+    if (nil != normalImage && nil != selectImage)
+    {
+        cell.backgroundView = [[[UIImageView alloc] initWithImage:normalImage]autorelease];
+        cell.selectedBackgroundView = [[[UIImageView alloc]initWithImage:selectImage]autorelease];
+    }
+    else
+    {
+        cell.backgroundView.hidden = YES;
+        cell.selectedBackgroundView.hidden = YES;
+    }
+    
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    //if ([[Util sharedUtil] hasLogin])
+    
+    UIViewController *controller;
+    
+    switch (indexPath.row)
+    {
+        case 0:
+            CustomLog(@"设置");
+            /*controller = [[CSSettingsViewController alloc] initWithNibName:@"CSSettingsViewController" bundle:nil];
+            [self.parentController.navigationController pushViewController:controller animated:YES];
+            [controller release];*/
+            break;
+        case 1:
+            CustomLog(@"意见反馈");
+            /*controller = [[CSFeedBackViewController alloc] initWithNibName:@"CSFeedBackViewController" bundle:nil];
+            [self.parentController.navigationController pushViewController:controller animated:YES];
+            [controller release];*/
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (IBAction)backButtonPressed:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+@end
